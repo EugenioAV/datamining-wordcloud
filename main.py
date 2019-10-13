0940ddd5095d01f2f06e3f037b56a76766543c8f
Eugenio <hedgehog0996@gmail.com>
2019-10-13 22:09:44 +0300

    added the mask

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 from wordcloud import WordCloud, STOPWORDS
 
 
@@ -10,17 +12,22 @@ def read_data(file_path='input/text.txt'):
     return text
 
 
+def get_mask(d, file_path='input/cloud_mask.png'):
+    return np.array(Image.open(os.path.join(d, file_path)))
+
+
 def draw_word_cloud(text):
     stopwords = set(STOPWORDS)
-
-    word_cloud = WordCloud(max_font_size=70, background_color="white", max_words=50, stopwords=stopwords)
-    word_cloud.generate(text)
-
     d = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+    cloud_mask = get_mask(d)
+
+    word_cloud = WordCloud(background_color="white", max_font_size=500, max_words=100,
+                           mask=cloud_mask, stopwords=stopwords)
+    word_cloud.generate(text)
     word_cloud.to_file(os.path.join(d, "output/word_cloud.png"))
 
     plt.figure()
-    plt.imshow(word_cloud, interpolation="bilinear")
+    plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
 
